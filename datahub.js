2faf1e303b18e21a17b0ad8c2ae820f70f37c807
Ji-Eun Kim <anotherph@gmail.com>
2024-02-14 14:55:21 +0900

    edits topic

--- datahub.js
+++ datahub.js
@@ -11,7 +11,7 @@ const client = mqtt.connect(connectUrl);
 // Topic
 let mobiusTopic = {
     // mobius: '/KETI_Flowmeter'
-    // mobius: '/'+`${conf.ae.name}`
+    //mobius: '/'+`${conf.ae.name}`
     mobius: '//Mobius/KETI_Flowmeter/flowmeter'
 };
 
